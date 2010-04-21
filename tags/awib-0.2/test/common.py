32064b6597ed3db5cfdbe60a04f9efaeb855f432
matslina <matslina@45282222-9dd6-11dd-93a9-677c3c677ea2>
2010-04-21 20:50:23 +0000

    tagging release 0.2

---
+++ tags/awib-0.2/test/common.py
@@ -0,0 +1,84 @@
+# -*- coding: utf-8 -*-
+
+import unittest
+
+from awip import AWIP, AWIPError, AWIPStepError
+
+def _diff_pos(a,b):
+    for i in xrange(min(len(a), len(b))):
+        if a[i] != b[i]:
+            return i
+    return -1
+
+class BFTestCase(unittest.TestCase):
+    def __init__(self, *args, **kwargs):
+        unittest.TestCase.__init__(self, *args, **kwargs)
+        self.__code = None
+
+    def run_bf(self, code, input, steps=50000,
+            check_output=True, check_memory=True, precondition=[], pointer=0):
+        """Runs code with different cell size and eof-behaviour.
+
+        @param input: program input
+        @param steps: number of AWIP IR operations before failing
+        @param check_output: assert output is equal regardless of environment
+        @param check_memory: assert memory post execution is equal ...
+        @param precondition: run program with this initial memory layout
+        @param pointer: run program with this initial pointer position
+        @rtype: ([], [])
+        @return: program output and memory post execution as: (output, memory)
+        """
+
+        if self.__code != code:
+            self._interpreter = AWIP(code)
+            self.__code = code
+
+        output, memory, prev_cs, prev_eb = None, None, None, None
+
+        for cell_size in (8,16,64):
+            for eof__code, eof_name in\
+                    ((AWIP.EOF_NO_CHANGE, "no change"),
+                     (AWIP.EOF_WRITE_ZERO, "write zero"),
+                     (AWIP.EOF_WRITE_MINUS_ONE, "write minus one")):
+
+                try:
+                    out, mem = self._interpreter.run(input=input,
+                                                     memory=precondition,
+                                                     cell_size=cell_size,
+                                                     eof_behaviour=eof__code,
+                                                     steps=steps,
+                                                     pointer=pointer)
+                except AWIPStepError:
+                    self.fail("code didn't terminate within %d steps for "
+                              "(%d bits, %s)" %
+                              (steps, cell_size, eof_name))
+                except AWIPError, ae:
+                    self.fail("code fails for (%d bits, %s): %s" %
+                              (cell_size, eof_name, ae))
+
+                if output is None:
+                    output = out
+                elif check_output and output != out:
+                    i = _diff_pos(output, out)
+                    self.fail("output differs between (%d bits, %s) and "
+                              "(%d bits, %s) in position %d: 0x%02x != 0x%02x" %
+                              (cell_size, eof_name, prev_cs, prev_eb,
+                               i, out[i], output[i]))
+
+                if memory is None:
+                    memory = mem
+                elif check_memory and memory != mem:
+                    i = _diff_pos(memory, mem)
+                    self.fail("memory (post exec) differs between "
+                              "(%d bits, %s) and (%d bits, %s) in position"
+                              " %d: 0x%02x != 0x%02x" %
+                              (cell_size, eof_name, prev_cs, prev_eb,
+                               i, mem[i], memory[i]))
+
+                prev_cs, prev_eb = cell_size, eof_name
+
+        for i in xrange(len(memory)-1, -1, -1):
+            if memory[i] != 0:
+                break
+
+        return (output, memory[:i+1])
---
+++ tags/awib-0.2/util/bfpp.py
@@ -0,0 +1,162 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+"""Simple brainfuck preprocessor.
+
+The preprocessor performs comment removal and pretty-printing of brainfuck
+source code. It also implements an include statement for building programs
+from multiple source files.
+
+By default, pretty-printing consists printing the preprocessed code with a
+78-character line width. Optionally, a format file describing a more
+elaborate formatting of the stripped code can be specified. If so, the
+content of the format file is printed with the i:th non-whitespace character
+replaced with the i:th brainfuck instruction from the stripped source code.
+
+The include statement is expected to appear alone on a single line and
+be on the form
+
+ #include(FILENAME)
+
+where FILENAME is the path of the file to include. The path must be absolute
+or relative to the file holding the include statement.
+
+2008-03-23
+Mats Linander
+"""
+
+import sys
+import re
+import optparse
+import os
+
+include_re = re.compile(r'\s*#include\(([^)]+)\)')
+BFIS = set([',','.','-','+','[',']','<','>'])
+
+def preprocess(filename):
+    """Preprocesses brainfuck sourcecode.
+
+    Comments are stripped and files are included.
+    """
+
+    if filename:
+        file = open(filename,"r")
+        directory = os.path.split(filename)[0]
+    else:
+        file = sys.stdin
+        directory = os.getcwd()
+
+    data = []
+    for line in file:
+        m = include_re.match(line)
+        if m:
+            line = preprocess(os.path.join(directory, m.group(1)))
+        data.append(''.join(c for c in line if c in BFIS))
+    return ''.join(data)
+
+def format(code, width=78, formatfile=None):
+    """Formats preprocessed code for pretty-printing."""
+
+    if formatfile is None:
+        return [code[j:j+width] for j in xrange(0, len(code), width)]
+
+    out, line, code = list(), list(), list(code)
+    for f in open(formatfile).read()[:len(code)]:
+        if f == ' ':
+            line.append(' ')
+        elif f == '\n':
+            out.append(''.join(line))
+            line = []
+        else:
+            line.append(code.pop(0))
+    if line:
+        out.append(''.join(line))
+
+    return out + [''.join(code[j:j+width]) for j in xrange(0,len(code),width)]
+
+def attach_interpreter(code):
+    stuff = {}
+
+    # assumes 'char *K' points at code to run
+    stuff['c_interpreter'] =\
+        ("#define _K_(___,_K,__)(K[_KK]^(___))?_K_^_K_:"
+         "_K _K_ __;\n_K_,_KK,__K,____,__[4>>002<<020];"
+         "_(__){__K=____?__K:__;____=__;while(__K){__K+"
+         "=*(__*__*__+_KK+K)-0133?22^_KK[K+__]^'K'?'.'^"
+         "'u'^'[':-(__[_KK+K]&1):__*__;_KK+=__;_(__);}}"
+         "___(){while(K[++_KK]){    _K_('v'^']',__[,]++"
+         ")_K_(055|___==___,__[,]--)_K_('K'^'u',,++)_K_"
+         "('w'^'K',--,)_K_('_'^4,__[,]?_(_KK[K]&1>>1):_"
+         "(_KK[K]&1))_K_('v'^'+',__[,]?_((_KK[K]&2)-(_K"
+         "K[K]&1)):_(0))K[_KK]-46?_(__[_K_]&1>>1):putch"
+         "ar(__[_K_]);if(_KK[K]^(1<<1^'.'))_(0);else{__"
+         "[_K_]=getchar();}}}main(){--_KK;___();}")
+
+    # assumes $KK holds code to run
+    stuff['bash_interpreter'] =\
+        (r'O0=($(echo "$KK"|sed -e"s/\(.\)/\\1 /g"));OO'
+         r'=($(for ((i=0;i<65535;i++));do echo "0";done'
+         r'));Oo=0;oo="";oO=0;o0=0;while [ $oO -lt ${#O'
+         r'0[@]} ]; do case ${O0[$oO]} in ">")o0=$((o0+'
+         r'1));;"<")o0=$((o0-1));;"+")OO[$o0]=$(((${OO['
+         r'$o0]}+1)%0x100));;"-")OO[$o0]=$(((${OO[$o0]}'
+         r'+0377)%0x100));;".")echo -ne "\0$(((${OO[$o0'
+         r']}/64)%8))$(((${OO[o0]}/8)%8))$((${OO[o0]}%8'
+         r'))";;",")if ((${#oo}==0)); then oo=$(cat);fi'
+         r';if ((${#oo}>0)); then OO[$o0]=$(printf "%d"'
+          ' "\'${oo:0:1}");oo=${oo:1};fi;;"[")if ((${OO'
+         r'[$o0]}==0));then c=1;while (($c>0));do oO=$('
+         r'($oO+1));if [ ${O0[$oO]} == \[ ];then c=$((c'
+         r'+1));fi;if [ ${O0[$oO]} == \] ]; then ((c--)'
+         r');fi;done;else O0O[$Oo]=$oO;((Oo++));fi;;"]"'
+         r')((Oo--));if ((${OO[$o0]}!=0)); then oO=$(($'
+         r'{O0O[$Oo]}-1));fi;;esac;((oO++));done;')
+
+    stuff['bf_code'] = ' \\\n'.join(code)
+
+    return ['#define KK char *K\n'
+            '#define true char *K\n'
+            'true;\n'
+            '#define EOF /*\n'
+            'cat>>/dev/null<<EOF\n'
+            '[*/\n'
+            '%(c_interpreter)s\n'
+            '/*]*/\n'
+            'EOF\n'
+            '\n'
+            'KK="\\\n'
+            '%(bf_code)s";\n'
+            '\n'
+            '#if 0/*[\n'
+            '%(bash_interpreter)s\n'
+            '#]*/\n'
+            '#endif\n' % stuff]
+
+def main():
+    parser = optparse.OptionParser(usage="%prog [options] FILE")
+    parser.add_option("-t", "--format",
+                      dest="format", metavar="F", default=None,
+                      help="write output according to format in file F")
+    parser.add_option("-w", "--width",
+                      dest="width", type="int", metavar="W",default=78,
+                      help="write output using line width W (default 78)")
+    parser.add_option('', '--interpreter',
+                      dest="interpreter", action='store_true',default=False,
+                      help="attach C and bash interpreters")
+    (options, args) = parser.parse_args()
+
+    filename = None
+    if len(args) > 0 and args[0] != '-':
+        filename = os.path.abspath(args[0])
+
+    code = preprocess(filename)
+    code = format(code, options.width, options.format)
+    if options.interpreter:
+        code = attach_interpreter(code)
+    sys.stdout.write('\n'.join(code))
+    sys.stdout.write('\n')
+
+    return 0
+
+if __name__ == "__main__":
+    sys.exit(main())
